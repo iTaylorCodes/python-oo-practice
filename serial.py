68c8726c87476753e79ea8ff467e7b77d19906f2
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-05-16 16:03:08 -0700

    Added methods to SerialGenerator class

--- serial.py
+++ serial.py
@@ -20,3 +20,23 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start = 0):
+        """Make a sequencial number generator, starting from start."""
+
+        self.start = self.next = start
+
+    def __repr__(self):
+        """Shows representation."""
+
+        return f"SerialGenerator(start={self.start}, next={self.next})"
+
+    def generate(self):
+        """Generates next number in sequenc."""
+
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """Resets generated number to starting position."""
+
+        self.next = self.start
